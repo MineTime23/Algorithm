9ebfa9f51dcae237bf5952b9dadabc66bc6eaffc
Miner <100114639+miner61@users.noreply.github.com>
2023-07-13 21:46:33 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,14 @@
+def solution(num_list):
+    even = ""
+    odd = ""
+    for i in num_list:
+        if i % 2 == 0:
+            even += str(i)
+        else:
+            odd += str(i)
+    if even and odd:
+        return int(even)+int(odd)
+    elif even:
+        return int(even)
+    elif odd:
+        return int(odd)
