5ed5e48ac560c7c28ae77c87a7f866c6ca04bdeb
Miner <100114639+miner61@users.noreply.github.com>
2023-07-13 23:10:24 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -2,7 +2,9 @@ def solution(n):
     answer = 0
     if n == 0:
         return 0
-    for i in range(1,n+1):
+    for i in range(1,int(n**0.5)+1):
         if n % i == 0:
-            answer += i
+            answer += (n//i + i)
+    if int(n**0.5) == n**0.5:
+        answer -= (n**0.5)
     return answer
