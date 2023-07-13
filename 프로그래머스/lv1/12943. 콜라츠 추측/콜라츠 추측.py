30c4bf53810b187df6fe5b2e010580e3ef2d9d2b
Miner <100114639+miner61@users.noreply.github.com>
2023-07-13 23:58:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,12 @@
+def solution(num):
+    answer = 0
+    while answer != 501 and num != 1:
+        answer += 1
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = num * 3 + 1
+    if num == 1:
+        return answer
+    elif answer == 501:
+        return -1
