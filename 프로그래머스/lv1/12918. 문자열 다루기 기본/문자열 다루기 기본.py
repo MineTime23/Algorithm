53cd1af4edc6d4d3707c247755c97e4b4cc6a649
Miner <100114639+miner61@users.noreply.github.com>
2023-07-20 00:09:58 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,7 +1,2 @@
 def solution(s):
-    if len(s) != 4 and len(s) != 6:
-        return False
-    for i in s:
-        if i.isalpha():
-            return False
-    return True
+    return s.isdigit() and len(s) in [4,6]
