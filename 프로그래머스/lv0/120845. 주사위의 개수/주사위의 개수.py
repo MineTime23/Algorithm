d8b4eeecb05e33e78731fd148d05aa1a8cc3de4e
Miner <100114639+miner61@users.noreply.github.com>
2023-07-20 11:54:17 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,3 +1,3 @@
 def solution(box, n):
-    box = list(map(lambda x : str(x//n),box))
-    return int(eval('*'.join(box)))
+    box = list(map(lambda x : x//n,box))
+    return box[0] * box[1] * box[2]
