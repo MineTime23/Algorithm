34c655417af77e7fe72f3b398156bcc2aa9d9168
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-21 19:25:30 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,6 +1,6 @@
 def solution(n):
-    answer = ""
+    result = []
     while n != 0:
-        answer += f"{n%3}"
-        n = n // 3
-    return int(answer,3)
+        result.append(n%3)
+        n//= 3
+    return int(''.join(map(str, result)),3)
