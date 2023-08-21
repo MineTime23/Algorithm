a46babd7f3a90f5adda8744562b023c35a92ee55
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-21 20:15:15 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,5 +1,5 @@
 def solution(s):
-    arr_a = ["zero","one","two","three","four","five","six","seven","eight","nine"]
-    for i in range(len(arr_a)):
-        s =s.replace(arr_a[i],f"{i}")
+    num = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    for i in range(len(num)):
+        s = s.replace(num[i],str(i))
     return int(s)
