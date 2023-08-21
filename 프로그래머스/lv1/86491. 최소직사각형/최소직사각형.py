ffc953b3a56152c26ea5ac611528546f3bd0aeef
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-21 19:58:02 +0900

    [level 1] Title: 최소직사각형, Time: 3.22 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,9 +1,9 @@
 def solution(sizes):
-    max_num = 0
-    min_num = 0
+    max_n = 0
+    min_n = 0
     for a,b in sizes:
-        if max_num < max(a,b):
-            max_num = max(a,b)
-        if min_num < min(a,b):
-            min_num = min(a,b)
-    return max_num * min_num
+        if max(a,b) > max_n:
+            max_n = max(a,b)
+        if min(a,b) > min_n:
+            min_n = min(a,b)
+    return max_n * min_n
