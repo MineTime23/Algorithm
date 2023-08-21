ced2ebd5222273cf0a6f8774b0aacfbb78b56055
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-21 16:49:04 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,11 +1,12 @@
 def solution(num):
     answer = 0
-    while answer != 501 and num != 1:
-        answer += 1
+    while num != 1:
         if num % 2 == 0:
             num //= 2
+            answer += 1
         else:
-            num = num * 3 + 1
-    if num == 1:
-        return answer
-    return -1
+            num = 3 * num + 1
+            answer += 1
+        if answer > 500:
+            return -1
+    return answer
