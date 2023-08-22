62c8fed70c23d38b78637bfe75ab8f37224518e5
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-22 22:50:14 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,13 +1,11 @@
 def solution(cards1, cards2, goal):
-    cnt = 0
-    while cnt != (len(goal)):
-        if cards1 and goal[cnt] == cards1[0]:
-            cards1 = cards1[1:]
-        elif cards2 and goal[cnt] == cards2[0]:
-            cards2 = cards2[1:]
+    a = 0
+    b = 0
+    for i in goal:
+        if a<= len(cards1)-1 and i == cards1[a]:
+            a += 1
+        elif b<= len(cards2)-1 and i == cards2[b]:
+            b += 1
         else:
             return "No"
-        cnt += 1    
     return "Yes"
-
-solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"])
