368e972a710f6b3ce25aee6d0fa57da57395a158
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-22 22:39:30 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,7 +1,4 @@
 def solution(a, b):
-    day = ["FRI","SAT","SUN","MON","TUE","WED","THU"]
-    month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
-    da = b
-    for i in range(a):
-        da += month[i]
-    return day[(da-1)%7]
+    days = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+    weekday = ["FRI","SAT","SUN","MON","TUE","WED","THU"]
+    return weekday[(sum(days[:a]) + b-1)%7]
