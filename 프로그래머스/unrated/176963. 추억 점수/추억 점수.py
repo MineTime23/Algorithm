2654aff5b054369870e49b4558f79f1876c6aee0
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-22 22:12:33 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -2,11 +2,8 @@ from collections import defaultdict
 def solution(name, yearning, photo):
     answer = []
     data = defaultdict(int)
-    for i in range(len(name)):
-        data[name[i]] = yearning[i]
+    for a, b in zip(name, yearning):
+        data[a] = b
     for i in photo:
-        total = 0
-        for j in i:
-            total += data[j]
-        answer.append(total)    
+        answer.append(sum(map(lambda x : data[x],i)))     
     return answer
