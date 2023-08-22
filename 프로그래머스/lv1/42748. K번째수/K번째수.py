3bd30409c583cbdede16bc98cae6f7898018709a
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-22 21:31:50 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,5 +1,2 @@
 def solution(array, commands):
-    answer = []
-    for a,b,m in commands:
-        answer.append(sorted(array[a-1:b])[m-1])
-    return answer
+    return [sorted(array[a-1:b])[c-1] for a,b,c in commands]
