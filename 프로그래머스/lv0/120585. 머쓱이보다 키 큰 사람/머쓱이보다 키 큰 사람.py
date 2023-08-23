d4eb62c04f8aa6090492e4572761a09d82e71730
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-23 18:38:17 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,6 +1,2 @@
 def solution(array, height):
-    answer = 0
-    for i in array:
-        if i > height:
-            answer += 1
-    return answer
+    return sum([1 for i in array if i > height])
