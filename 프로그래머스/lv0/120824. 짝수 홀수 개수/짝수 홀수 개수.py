a7361a1dc3fedb11384b1ec3cbfae138b94207df
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-23 18:33:39 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,7 +1,7 @@
 def solution(num_list):
     answer = [0,0]
     for i in num_list:
-        if i % 2 == 0:
+        if i%2 == 0:
             answer[0] += 1
         else:
             answer[1] += 1
