b86087c5b90ead8778e99f1fa2871e19e4bffbe8
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-23 20:19:27 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,15 +1,16 @@
-import itertools 
-def primeNum(n):
-    if n == 2 or n == 3:
+import itertools
+
+def primenum(num):
+    if num == 2:
         return True
-    for i in range(2,int(n**0.5)+1):
-        if n % i == 0:
+    for i in range(2,int(num**0.5)+1):
+        if num % i == 0:
             return False
     return True
-        
+
 def solution(nums):
     answer = 0
     for i in itertools.combinations(nums,3):
-        if primeNum(sum(i)):
+        if primenum(sum(i)):
             answer += 1
     return answer
