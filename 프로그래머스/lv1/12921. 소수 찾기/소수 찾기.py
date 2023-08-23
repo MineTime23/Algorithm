41596c47194e2ce9eaefbd082d8b251351657f8a
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-23 20:24:11 +0900

    [level 1] Title: 소수 찾기, Time: 3282.94 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,16 +1,14 @@
-def primeNum(n):
-    if n == 1:
-        return False
-    if n == 2 or n == 3:
+def primenum(num):
+    if num == 2:
         return True
-    for i in range(2, int(n**0.5)+1):
-        if n%i == 0:
+    for i in range(2,int(num**0.5)+1):
+        if num % i == 0:
             return False
     return True
 
 def solution(n):
     answer = 0
-    for i in range(1,n+1):
-        if primeNum(i):
+    for i in range(2,n+1):
+        if primenum(i):
             answer += 1
     return answer
