8abd8aa04b6fdf535e028c748952347ce6400530
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-23 19:10:37 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,6 +1,4 @@
+import re
 def solution(my_string):
-    answer = 0
-    for i in my_string:
-        if i.isnumeric():
-            answer += int(i)
-    return answer
+    my_string = re.sub(r'[a-zA-Z]','',my_string)
+    return sum(map(int,list(my_string)))
