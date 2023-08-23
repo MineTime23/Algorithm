6f6e46d4daa9dbf4015b22809b7e6365affd2b86
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-23 18:55:48 +0900

    [level 0] Title: 순서쌍의 개수, Time: 55.82 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,8 +1,2 @@
 def solution(n):
-    answer = 0
-    if n == 1:
-        return 1
-    for i in range(1, n+1):
-        if n%i == 0:
-            answer += 1
-    return answer
+    return sum([1 for i in range(1,n+1) if n%i == 0])
