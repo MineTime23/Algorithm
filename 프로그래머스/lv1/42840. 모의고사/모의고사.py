52fb9b8d93545f53eae8ec52347f72db72e5ec4c
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-23 20:15:47 +0900

    [level 1] Title: 모의고사, Time: 2.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,14 +1,14 @@
 def solution(answers):
-    arr_1 = [1, 2, 3, 4, 5]
-    arr_2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    arr_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    score = [0, 0, 0, 0]
+    n1 = [1, 2, 3, 4, 5]
+    n2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    n3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    result = [0,0,0,0]
     for i in range(len(answers)):
-        if answers[i] == arr_1[i%5]:
-            score[1] += 1
-        if answers[i] == arr_2[i%8]:
-            score[2] += 1
-        if answers[i] == arr_3[i%10]:
-            score[3] += 1
-    answer = [i for i in range(1,len(score)) if score[i] == max(score)]     
+        if answers[i] == n1[i%5]:
+            result[1] += 1
+        if answers[i] == n2[i%8]:
+            result[2] += 1
+        if answers[i] == n3[i%10]:
+            result[3] += 1
+    answer = [i for i in range(len(result)) if result[i] == max(result)]
     return sorted(answer)
