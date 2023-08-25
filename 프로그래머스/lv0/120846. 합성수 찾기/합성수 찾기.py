a6529d16ac311ba967fc455ff7d75d0636758622
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-25 15:08:48 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,16 +1,12 @@
-def cal(n):
-    for i in range(2,n):
+def prime(n):
+    for i in range(2, int(n**0.5)+1):
         if n % i == 0:
             return False
     return True
+
 def solution(n):
     answer = 0
-    for i in range(1,n+1):
-        if i == 1 or i == 2:
-            continue
-        else:
-            if cal(i):
-                continue
-            else:
-                answer += 1
+    for i in range(1, n+1):
+        if not prime(i):
+            answer += 1
     return answer
