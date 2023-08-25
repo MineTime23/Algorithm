7cab8cdd650b2098c8fb2933fd347e9c032be3a4
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-25 14:40:55 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,3 +1,6 @@
 def solution(rsp):
-    data = {"2":"0","0":"5","5":"2"}
-    return ''.join(map(lambda x : data[x],rsp))
+    res = {"2": "0","0":"5","5":"2"}
+    answer = ""
+    for i in rsp:
+        answer += res[i]
+    return answer
