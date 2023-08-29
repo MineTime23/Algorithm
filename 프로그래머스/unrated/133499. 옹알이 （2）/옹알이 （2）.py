72c636017ae12872e988db00462018898e91c2df
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-29 17:47:27 +0900

    [unrated] Title: 옹알이 (2), Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,8 +1,14 @@
 def solution(babbling):
     answer = 0
-    for i in range(len(babbling)):
-        babbling[i] = babbling[i].replace("aya","1").replace("ye","2").replace("woo","3").replace("ma","4")
-        babbling[i] = babbling[i].replace("11","X").replace("22","X").replace("33","X").replace("44","X")
-        if babbling[i].isdigit() and "X" not in babbling[i]:
-            answer += 1
+    for i in babbling:
+        str1 = i.replace("aya","1").replace("ye","2").replace("woo","3").replace("ma","4")
+        if str1.isdigit():
+            if len(str1) == 1:
+                answer += 1
+            else:
+                for i in ["11","22","33","44"]:
+                    if i in str1:
+                        break
+                else:
+                    answer += 1
     return answer
