7ae81bb5f048539a247ce91407e474b677f816af
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-08-31 15:18:18 +0900

    [level 2] Title: H-Index, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,10 +1,9 @@
 from collections import Counter
 def solution(citations):
-    arrange = sorted(list(set(citations)))
     total = len(citations)
     cal = Counter(citations)
     answer = 0
-    for i in range(max(arrange)+1):
+    for i in range(max(citations)+1):
         if total >= i:
             answer = i
         total -= cal[i]
