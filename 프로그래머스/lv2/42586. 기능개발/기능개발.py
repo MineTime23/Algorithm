4fe80b809484b62415553119bb5ef4706f7e2f19
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-01 21:19:58 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+def solution(progresses, speeds):
+    progresses = list(map(lambda x : 100 - x, progresses))
+    #print(progresses)
+    progresses = [ progresses[i]//speeds[i] if progresses[i] % speeds[i] == 0 else progresses[i]//speeds[i] + 1 for i in range(len(progresses))]
+    #print(progresses)
+    for i in range(len(progresses)):
+        if i == 0: 
+            continue
+        else:
+            if progresses[i] < progresses[i-1]:
+                progresses[i] = progresses[i-1]
+    return list(Counter(progresses).values())
