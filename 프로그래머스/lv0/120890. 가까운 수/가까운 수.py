9d70b39199b589e2ae2082e246f7091d5c55ad30
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-04 13:04:05 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,17 +1,4 @@
 def solution(array, n):
-    answer = []
-    array.append(n)
     array.sort()
-    idx = array.index(n)
-    if idx-1 >= 0 and idx-1 < len(array):
-        answer.append(array[idx-1])
-    if idx+1 >= 0 and idx+1 < len(array):
-        answer.append(array[idx+1])
-    if len(answer) == 1:
-        return answer[0]
-    else:
-        if abs(answer[0]-n) <= abs(answer[1]-n):
-            return answer[0]
-        else:
-            return answer[1]
-    return answer
+    array = sorted(array, key = lambda x : (abs(x-n),-n))
+    return array[0]
