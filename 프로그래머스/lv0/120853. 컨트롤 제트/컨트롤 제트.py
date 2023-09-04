2dd70e9cde97e738baab269a5cd231dae79866cd
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-04 13:38:51 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,9 +1,10 @@
 def solution(s):
     answer = 0
-    s = s.split()
-    for i,v in enumerate(s):
-        if (i != len(s)-1 and s[i+1] == "Z") or v == "Z":
-            continue
+    s =s.split()
+    stack = []
+    for i in s:
+        if i == "Z":
+            stack.pop()
         else:
-            answer += int(v)
-    return answer
+            stack.append(int(i))
+    return sum(stack)
