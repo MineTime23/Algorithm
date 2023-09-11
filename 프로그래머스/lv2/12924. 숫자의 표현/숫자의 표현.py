4b0a9a9fdc6a7e200b8097419aeee794cb24eb1f
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-11 23:44:21 +0900

    [level 2] Title: 숫자의 표현, Time: 0.80 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,16 +1,14 @@
 def solution(n):
-    
+    answer = 2
     if n == 1:
         return 1
     if n == 2:
         return 1
-
-    
-    answer = 1
-    if n%2 == 1:
-        answer += 1
-    for i in range(3,n,2):
-        if n%i == 0:
-            answer += 1
-            
+    if n == 3:
+        return 2
+    for i in range(2,n):
+        if n % i == 0 and i % 2 == 1:
+            answer += 1 
+    if n % 2 == 0:
+        answer -= 1
     return answer
