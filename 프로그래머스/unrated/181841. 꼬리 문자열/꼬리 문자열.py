2362d48cc21bed6b7eca3563bee087ad5566bffa
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-13 20:42:02 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,2 +1,6 @@
 def solution(str_list, ex):
-    return ''.join([i for i in str_list if not ex in i])
+    answer = ''
+    for i in str_list:
+        if not ex in i:
+            answer += i
+    return answer
