fa029924a0beb2b8d9acab48829938dc403e7329
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-13 20:45:28 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
@@ -1,7 +1,8 @@
 def solution(a, b):
-    if a % 2 == 0 and b % 2 == 0:
+    answer = 0
+    if a % 2 == 1 and b % 2 == 1:
+        return a**2 + b**2
+    elif a % 2 == 0 and b % 2 == 0:
         return abs(a-b)
-    elif a % 2 == 1 and b % 2 == 1:
-        return a**2 + b**2 
     else:
-        return (a+b) * 2
+        return 2 * (a+b)
