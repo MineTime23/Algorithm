3ef3b79fdefbc787d6ffd8badd415800f11e620b
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-13 20:05:49 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,5 +1,3 @@
 def solution(num_list):
-    a = sum(num_list)
-    num_list = map(str, num_list)
-    arr = int(eval("*".join(num_list)))
-    return 1 if arr < a**2 else 0
+    answer = 0
+    return int(eval("*".join(map(str,num_list)))< sum(num_list)**2 )
