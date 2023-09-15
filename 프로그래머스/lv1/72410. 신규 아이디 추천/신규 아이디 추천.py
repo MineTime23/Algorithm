becd2e057508fae8ebb21e6055b8fabd7bd711b8
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-15 15:02:54 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,17 +1,19 @@
 import re
-
 def solution(new_id):
+    answer = ''
     new_id = new_id.lower()
-    new_id = re.sub('[^a-z0-9-_.]', "", new_id)
-    new_id = re.sub('(\.+)',".",new_id)
+    new_id = re.sub("[^a-z0-9-_.]","",new_id)
+    new_id = re.sub("[.]+",".",new_id)
+    if new_id.endswith("."):
+        new_id = new_id[:-1]
     if new_id.startswith("."):
         new_id = new_id[1:]
-    if not new_id:
-        new_id = "a"
+    if len(new_id) == 0:
+        new_id += "a"
     if len(new_id) >= 16:
         new_id = new_id[:15]
     if new_id.endswith("."):
-        new_id =new_id[:-1]
+        new_id = new_id[:-1]   
     if len(new_id) <= 2:
         while len(new_id) != 3:
             new_id += new_id[-1]
