11cc1168182b5ce5a5a30771aa314edd3f54a19c
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-16 17:14:15 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,2 +1,5 @@
 def solution(arr, idx):
-    return -1 if not 1 in arr[idx:] else arr[idx:].index(1)+idx
+    for i in range(idx,len(arr)):
+        if arr[i] == 1:
+            return i
+    return -1
