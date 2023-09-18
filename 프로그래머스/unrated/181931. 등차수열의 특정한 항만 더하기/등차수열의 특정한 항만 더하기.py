ea0497f74e52eafe09263303ade9d9b09904d13b
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-18 19:49:00 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,6 +1,7 @@
 def solution(a, d, included):
     answer = 0
     for i in range(len(included)):
+        res = a + i*d
         if included[i]:
-            answer += (a + i*d)
+            answer += res
     return answer
