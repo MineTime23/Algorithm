4b91d2086dc3b484cc58864951268beea8c4dc4f
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-09-18 20:00:42 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 6.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -1,5 +1,6 @@
+
 def solution(arr, queries):
     for a,b in queries:
-        for j in range(a,b+1):
-            arr[j] += 1
+        for i in range(a, b+1):
+            arr[i] += 1
     return arr
