9f6c19de61d16128ed1983b8fcdea094c38928c0
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-11-23 19:14:09 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,14 +1,4 @@
 def solution(date1, date2):
-    if date1[0] < date2[0]:
-        return 1
-    elif date1[0] > date2[0]:
-        return 0
-    if date1[1] < date2[1]:
-        return 1
-    elif date1[1] > date2[1]:
-        return 0
-    if date1[2] < date2[2]:
-        return 1
-    elif date1[2] > date2[2]:
-        return 0
-    return 0
+    data1 = int(''.join(list(map(str,date1))))
+    data2 = int(''.join(list(map(str,date2))))
+    return int(data1 < data2)
