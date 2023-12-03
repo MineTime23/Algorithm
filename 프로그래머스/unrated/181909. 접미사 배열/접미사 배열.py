c53210cf64eb15c8f41add06709c3d764bd3844d
MineTime <100114639+MineTime23@users.noreply.github.com>
2023-12-03 12:48:32 +0900

    [level unrated] Title: 접미사 배열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
@@ -1,2 +1,6 @@
 def solution(my_string):
-    return sorted([my_string[i:] for i in range(len(my_string))])
+    answer = [
+        my_string[i:]
+        for i in range(len(my_string))
+    ]
+    return sorted(answer)
