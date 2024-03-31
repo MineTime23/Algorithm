61ac10d18cecb3b8ddc7316643e690f33d1106f0
MineTime <100114639+MineTime23@users.noreply.github.com>
2024-03-31 14:59:59 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.76 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -4,11 +4,13 @@ def solution(s):
     answer = []
     s = " " + s
     data = defaultdict(int)
-    for i,v in enumerate(s):
-        if data[v] == 0:
-            data[v] = i
+    for i,a in enumerate(s):
+        if a == " ":
+            continue
+        elif data[a] == 0:
             answer.append(-1)
+            data[a] = i
         else:
-            answer.append(i-data[v])
-            data[v] = i
-    return answer[1:]
+            answer.append(i-data[a])
+            data[a] = i
+    return answer
