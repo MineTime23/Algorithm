990c387a24cffb2800d5eae85e682b62d0a5b070
MineTime <100114639+MineTime23@users.noreply.github.com>
2024-03-31 15:04:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,6 +1,16 @@
-trans = {"zero":0,"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
-
 def solution(s):
-    for a in trans.items():
-        s = s.replace(a[0],str(a[1]))
+    trans = {
+        "zero" : "0",
+        "one" : "1",
+        "two" : "2",
+        "three" : "3",
+        "four" : "4",
+        "five" : "5",
+        "six" : "6",
+        "seven" : "7",
+        "eight" : "8",
+        "nine" : "9"
+    }
+    for a,b in trans.items():
+        s = s.replace(a,b)
     return int(s)
